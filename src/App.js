dd1b93bfa92df3e91714d7eeee5045943895fc32
sabuha99 <qemberova1999@mail.ru>
2022-12-20 02:55:32 -0800

    add react-router-dom

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Counter from "components/Counter";
+import HomePage from "pages/Home";
+import { Route, Routes} from "react-router-dom";
+import Users from "./components/Users";
+
+function App(){
+    return (
+    <>
+    <Routes>
+    <Route path="/" element={<HomePage/>} />
+      <Route path="/counter" element={<Counter />}/>
+      <Route path="users" element={<Users />}/>
+     
+    </Routes>
+        </>
+    );
+  }
+  export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-// import React from `react`
-import { createRoot } from "react-dom/client"
-import App from "./App"
-const root = createRoot(document.getElementById(`root`))
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-root.render(<App />)
+const root =createRoot(document.getElementById("root"));
+root.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+    );
